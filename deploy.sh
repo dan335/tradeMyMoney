f365ee5d7a3a1bdaffac7c1bcc5247f0d180831b
Dan <danphi@gmail.com>
2019-06-24 20:48:45 -0700

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ rm -rf build && mkdir build
 cp package.json build
 cp package-lock.json build
 
-docker build -t registry.gitlab.com/danphi/tradeMyMoney .
-docker push registry.gitlab.com/danphi/tradeMyMoney
+docker build -t registry.gitlab.com/danphi/trademymoney .
+docker push registry.gitlab.com/danphi/trademymoney
